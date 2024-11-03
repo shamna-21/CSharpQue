b905dc527bd8ce90156b3fc0eaafabdcd8c6e39c
shamna <sherinshamna78@gmail.com>
2024-11-03 09:42:09 +0530

    Add project files.

---
+++ LogicalQue1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LogicalQue1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            var a=Console.ReadLine();
+            if (int.TryParse(a, out var b)) {
+                EvenOrOdd(b);
+            }
+            else
+            {
+               
+                Console.WriteLine("enter a valid number");
+            }
+           
+        }
+        static void EvenOrOdd(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}

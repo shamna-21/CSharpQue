31b44554158a35913f0a5efaed2f8ef8c5285adc
shamna-21 <103629961+shamna-21@users.noreply.github.com>
2024-11-06 06:00:51 +0530

    Add files via upload

---
+++ Pattern.cs
@@ -0,0 +1,18 @@
+﻿namespace Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          for(int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}

7cd1c47ac0c74865ee60e4c8b3dc0d9bad64037c
shamna-21 <103629961+shamna-21@users.noreply.github.com>
2024-11-09 19:55:33 +0530

    Add files via upload

---
+++ Arraysum.cs
@@ -0,0 +1,16 @@
+﻿namespace ArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4 };
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            Console.WriteLine("Sum of array elements: " + sum);
+        }
+    }
+}
